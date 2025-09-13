6b66d247123158bd116777f12c0fd9c2705e096a
vitorkolle <vitorkolle.dev@gmail.com>
2025-09-13 15:30:12 -0300

    Build: Rotas definidas

--- src/Client/src/main.jsx
+++ src/Client/src/main.jsx
@@ -1,11 +1,29 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
 import Login from "./Login/Login.jsx";
 import Cadastro from "./Cadastro/Cadastro.jsx";
 import Home from "./Home/Home.jsx";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/cadastro",
+    element: <Cadastro />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+]);
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Home/>
+    <RouterProvider router={router} />
   </StrictMode>
 );
